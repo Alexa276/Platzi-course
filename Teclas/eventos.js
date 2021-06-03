ca9c622849b1e0f5d796ff012560c51d813e6466
Alexa276 <AlexaReyesRojas02@gmail.com>
2021-06-02 21:40:30 -0400

    Dibujos con Flechas

--- Teclas/eventos.js
+++ Teclas/eventos.js
@@ -8,28 +8,52 @@ var teclas = {
 
 console.log("teclas");
 
-document.addEventListener("keyup", dibujarTeclado);
+document.addEventListener("keydown", dibujarTeclado);
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getcontext("2d");
+var x = 100;
+var y = 100;
+
+dibujarlinea("red",x-1,y-1,x+1, y+1,papel);
+
+
+function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal, lienzo) {
+   
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.linewidht = 3;
+    lienzo.moveto (xinicial,yinicial);
+    lienzo.leneto(xfinal,yfinal);
+    lienzo.stroke;
+    lienzo.closePath();
+}
+
 
 function dibujarTeclado(evento){
 
-        switch(evento.keycode)
+        
+          var colorcito ="green";
+          var movimiento = 1;
+          switch(evento.keycode)
 
         {
             case teclas.UP:
-                console.log("arriba");
+                dibujarlinea(colorcito, x, y , x , y - movimiento, papel);
+                y = y - movimiento;
             break;
             case teclas.DOWN:
-                console.log("abajo");
+                dibujarlinea(colorcito, x, y , x , y + movimiento, papel);
+                y = y + movimiento;
             break;
             case teclas.LEFT:
-                console.log("izquierda");
+                dibujarlinea(colorcito, x, y , x - movimiento, y ,papel);
+                x = x - movimiento;
             break;
             case teclas.RIGHT:
-                console.log("derecha");
-            default:
-                console.log("otra tecla");
+                dibujarlinea(colorcito, x, y , x + movimiento, y ,papel);
+                x = x + movimiento;
+
             break;
         }
-    
 
     }
